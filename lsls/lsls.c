f9334cbae541f3dba507e1364f2732448eac3ea4
Jacob Seppanen <jbseppanen@gmail.com>
2019-05-31 08:58:09 -0700

    Prints filenames of current directory.

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,18 +1,32 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <sys/stat.h>
+#include <stdlib.h>
 
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
+
   // Parse command line
 
   // Open directory
+  struct dirent *d;
+  DIR *dircty = opendir(".");
+  if (dircty == NULL)
+  {
+    printf("Cannot open that directory");
+    exit(0);
+  }
 
   // Repeatly read and print entries
+  while ((d = readdir(dircty)) != NULL)
+  {
+    printf("%s\n", d->d_name);
+  }
 
   // Close directory
-
+  closedir(dircty);
   return 0;
 }
