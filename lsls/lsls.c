c02e890b03b306e46bb9d6c275da5b925b15f28c
Jacob Seppanen <jbseppanen@gmail.com>
2019-05-31 09:16:48 -0700

    Takes in directory argument.  MVP.

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -10,22 +10,31 @@ int main(int argc, char **argv)
 {
 
   // Parse command line
+  char *path;
+  if (argc>1)  //Make sure there is more than just the main argument.
+  {
+    path = argv[1];  // Get the first input argument after the main arg.  Ignore any extra args.
+  } else {
+    path = ".";
+  }
+  
+  printf("Input path is: %s\n", path);
 
   // Open directory
   struct dirent *d;
-  DIR *dircty = opendir(".");
+  DIR *dircty = opendir(path);
   if (dircty == NULL)
   {
-    printf("Cannot open that directory");
+    printf("Cannot open that directory.\n");
     exit(0);
   }
 
-  // Repeatly read and print entries
+  // Repeatedly read and print entries
   while ((d = readdir(dircty)) != NULL)
   {
     struct stat buf;
     stat(d->d_name, &buf);
-    printf("%lld %s\n", buf.st_size, d->d_name);
+    printf("%ld %s\n", buf.st_size, d->d_name);
   }
 
   // Close directory
