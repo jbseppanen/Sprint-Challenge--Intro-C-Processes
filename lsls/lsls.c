f865b275f43cef29678f982fbf785765e79967a3
Jacob Seppanen <jbseppanen@gmail.com>
2019-05-31 11:13:05 -0700

    Show <DIR> for directories.

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -2,6 +2,7 @@
 #include <dirent.h>
 #include <sys/stat.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * Main
@@ -11,13 +12,15 @@ int main(int argc, char **argv)
 
   // Parse command line
   char *path;
-  if (argc>1)  //Make sure there is more than just the main argument.
+  if (argc > 1) //Make sure there is more than just the main argument.
   {
-    path = argv[1];  // Get the first input argument after the main arg.  Ignore any extra args.
-  } else {
+    path = argv[1]; // Get the first input argument after the main arg.  Ignore any extra args.
+  }
+  else
+  {
     path = ".";
   }
-  
+
   printf("Input path is: %s\n", path);
 
   // Open directory
@@ -29,12 +32,27 @@ int main(int argc, char **argv)
     exit(0);
   }
 
+  char *full_path;
   // Repeatedly read and print entries
   while ((d = readdir(dircty)) != NULL)
   {
     struct stat buf;
-    stat(d->d_name, &buf);
-    printf("%ld %s\n", buf.st_size, d->d_name);
+    full_path[0] = '\0'; //Make sure it is clear for each loop.
+    strcat(full_path, path);
+    strcat(full_path, "/");
+    strcat(full_path, d->d_name);
+    if (stat(full_path, &buf) == -1)
+    {
+      printf("%10s %s\n", "?", d->d_name);
+    }
+    else if (S_ISDIR(buf.st_mode) != 0)
+    {
+      printf("%10s %s\n", "<DIR>", d->d_name);
+    }
+    else
+    {
+      printf("%10ld %s\n", buf.st_size, d->d_name);
+    }
   }
 
   // Close directory
